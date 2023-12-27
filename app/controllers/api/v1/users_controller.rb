9115fabc386f861fc977487c20e4ea9ff55514bf
Marc raphael <marcraphael979@gmail.com>
2023-12-28 00:37:18 +0100

    Comment out the seesion as not implemented yet

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      # session[:user_id] = @user.id
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
