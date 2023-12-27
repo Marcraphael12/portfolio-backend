1f266d201324a463588c00a911f885f3e2ef2d06
Marc raphael <marcraphael979@gmail.com>
2023-12-28 00:15:16 +0100

    app/controllers/api/v1/users_controller: Create show action for a single user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,10 @@ class Api::V1::UsersController < ApplicationController
     render json: users
   end
 
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user
+  end
 
   private
 
