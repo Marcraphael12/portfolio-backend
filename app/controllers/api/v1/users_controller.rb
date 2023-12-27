9775a32e2ad07179799512a7aea805ef25f1fb06
Marc raphael <marcraphael979@gmail.com>
2023-12-28 00:14:12 +0100

    app/controllers/api/v1/users_controller:  Add user_params as private class method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,4 +8,10 @@ class Api::V1::UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
