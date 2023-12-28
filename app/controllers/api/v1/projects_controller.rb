e6ae20228430bd689eba171e5f307af8717d63f0
Marc raphael <marcraphael979@gmail.com>
2023-12-28 02:00:25 +0100

    Update projects_controller.rb

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,38 +3,36 @@ class Api::V1::ProjectsController < ApplicationController
     @project = Project.new(project_params)
     if @project.save
       # session[:project_id] = @project.id
-      render json: @project, status: :created
+      render json: project, status: :created
     else
-      render json: @project.errors, status: :unprocessable_entity
+      render json: project.errors, status: :unprocessable_entity
     end
   end
 
   def index
-    projects = Project.where(user_id: params(:user_id))
-    render json: projects
+    projects = Project.where(user_id: params[:user_id])
+
+    if projects.empty?
+      render json: 'user without project'
+    else
+      render json: projects
+    end
   end
 
   def show
-    project = Project.find_by(id: params[:id])
-    if project
-      render json: project
-    else
-      render json: 'project not found'
-    end
+    project = Project.find(params[:id])
+    render json: project
   end
 
   def destroy
-    project = Project.find_by(id: params[:id])
-    if project
-      project.destroy
-    else
-      render json: 'project not found'
-    end
+    project = Project.find(params[:id])
+    project.destroy
+    render json: { message: 'Project successfully destroyed' }, status: :ok
   end
 
   private
 
   def project_params
-    params.require(:project).permit(:name, :github, :live, :description, :likes)
+    params.require(:project).permit(:name, :github, :live, :description)
   end
 end
