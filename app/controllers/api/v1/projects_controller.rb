64f8cfe041a3f038a8873db5b2d71941e6a357b3
Marc raphael <marcraphael979@gmail.com>
2023-12-28 01:13:17 +0100

    app/controllers/api/v1/projects_controller: Add action Create with projects_params

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::ProjectsController < ApplicationController
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      # session[:project_id] = @project.id
+      render json: @project, status: :created
+    else
+      render json: @project.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :github, :live, :description, :likes)
+  end
 end
