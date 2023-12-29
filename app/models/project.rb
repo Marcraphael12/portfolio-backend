5065deb696fa0bc5d0aa74720c1111ec63db173e
Marc raphael <marcraphael979@gmail.com>
2023-12-29 13:07:05 +0100

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,6 @@ class Project < ApplicationRecord
 
   has_many :likes, dependent: :destroy
 
-  validates :likes, numericality: { only_integer: true, allow_nill: true },
-                    comparison: { greater_than_or_equal_to: 0, allow_nill: true }
+  validates :likes_counter, numericality: { only_integer: true, allow_nill: true },
+                            comparison: { greater_than_or_equal_to: 0, allow_nill: true }
 end
