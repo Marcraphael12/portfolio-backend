7fdfa224ec21276958da1f6128cf24df1b137ec3
Marc raphael <marcraphael979@gmail.com>
2023-12-31 13:31:21 +0100

    app/controllers/api/v1/articles_controller: create

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::ArticlesController < ApplicationController
+
+end
+
+# t.string 'title'
+# t.integer 'comments'
+# t.string 'image'
+# t.string 'description'
+# t.integer 'likes_counter', default: 0
