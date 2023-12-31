86cbd2b7a99fab474ea707c07c974a67720bf301
Marc raphael <marcraphael979@gmail.com>
2023-12-31 15:00:32 +0100

    app/controllers/api/v1/articles_controller: Add create action with article_params

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,23 @@
 class Api::V1::ArticlesController < ApplicationController
+  def create
+    author = User.find(params[:user_id])
+    article = author.articles.new(article_params)
+    # article = Article.new(article_params)
+    if article.save
+      # session[:article_id] = @article.id
+      render json: article, status: :created
+    else
+      render json: article.errors, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :image, :description)
+  end
 end
 
 # t.string 'title'
-# t.integer 'comments'
 # t.string 'image'
 # t.string 'description'
-# t.integer 'likes_counter', default: 0
