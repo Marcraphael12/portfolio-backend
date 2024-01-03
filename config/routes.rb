c936204bfb4e45663ed107d98b537947f4130465
Marc raphael <marcraphael979@gmail.com>
2024-01-03 01:46:11 +0100

    Install devise and generate model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   namespace :api do
     namespace :v1 do
+      defaults format: :json
       resources :users do
         resources :projects
         resources :articles
