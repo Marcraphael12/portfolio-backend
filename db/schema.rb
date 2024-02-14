98ee5eb27789b698ff4e91bf63e6f3bd08e328f4
Marc raphael <marcraphael979@gmail.com>
2024-02-14 20:52:52 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,89 +10,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_226_224_450) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_26_224450) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'articles', force: :cascade do |t|
-    t.string 'title'
-    t.string 'image'
-    t.string 'description'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_articles_on_user_id'
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "image"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
-  create_table 'experiences', force: :cascade do |t|
-    t.string 'position'
-    t.string 'society'
-    t.string 'period'
-    t.string 'type'
-    t.string 'logo'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_experiences_on_user_id'
+  create_table "experiences", force: :cascade do |t|
+    t.string "position"
+    t.string "society"
+    t.string "period"
+    t.string "type"
+    t.string "logo"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
-  create_table 'job_descriptions', force: :cascade do |t|
-    t.string 'text'
-    t.bigint 'experience_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['experience_id'], name: 'index_job_descriptions_on_experience_id'
+  create_table "job_descriptions", force: :cascade do |t|
+    t.string "text"
+    t.bigint "experience_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["experience_id"], name: "index_job_descriptions_on_experience_id"
   end
 
-  create_table 'projects', force: :cascade do |t|
-    t.string 'name'
-    t.string 'github'
-    t.string 'live'
-    t.string 'description'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_projects_on_user_id'
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "github"
+    t.string "live"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table 'skills', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_skills_on_user_id'
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_skills_on_user_id"
   end
 
-  create_table 'testimonials', force: :cascade do |t|
-    t.string 'name'
-    t.string 'message'
-    t.string 'photo'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_testimonials_on_user_id'
+  create_table "testimonials", force: :cascade do |t|
+    t.string "name"
+    t.string "message"
+    t.string "photo"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_testimonials_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'videos', force: :cascade do |t|
-    t.string 'title'
-    t.string 'link'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_videos_on_user_id'
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
-  add_foreign_key 'articles', 'users'
-  add_foreign_key 'experiences', 'users'
-  add_foreign_key 'job_descriptions', 'experiences'
-  add_foreign_key 'projects', 'users'
-  add_foreign_key 'skills', 'users'
-  add_foreign_key 'testimonials', 'users'
-  add_foreign_key 'videos', 'users'
+  add_foreign_key "articles", "users"
+  add_foreign_key "experiences", "users"
+  add_foreign_key "job_descriptions", "experiences"
+  add_foreign_key "projects", "users"
+  add_foreign_key "skills", "users"
+  add_foreign_key "testimonials", "users"
+  add_foreign_key "videos", "users"
 end
